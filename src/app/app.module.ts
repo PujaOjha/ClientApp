f046b9b576e530451cd730022f3ddd1742e55dff
Puja <kumarp63@>
2021-01-25 00:01:34 +0530

    ClaimApp InitialCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ClaimComponent } from './claim/claim.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavComponent,
+    HomeComponent,
+    ClaimComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model:any = {}
+  
+  @Output() cancelRegister = new EventEmitter();
+  constructor(private accountService : AccountService, private toast : ToastrService) { }
+
+  ngOnInit(): void {
+  }
+  
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+
+  register()
+  {
+    this.accountService.register(this.model).subscribe( response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+      this.toast.error(JSON.stringify(error.error));
+      
+    }
+    );
+  }
+
+}
